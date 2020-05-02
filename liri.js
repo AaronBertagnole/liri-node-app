ba3c5c2cbbccfca3fe31f463973b76de8228bf0d
ASuicidalCow <aaron.bertagnole@gmail.com>
2020-05-01 21:03:50 -0600

    refactored some code to make bandsintown to display correctly

--- liri.js
+++ liri.js
@@ -1,60 +1,101 @@
-require("dotenv").config()
-var keys = require("./keys.js")
-var axios = require("axios")
-var Spotify = require("node-spotify-api")
-var dateFormat = require("date-format")
-var fs = require("fs")
-
+require("dotenv").config();
+var keys = require("./keys.js");
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
+var moment = require("moment");
+var fs = require("fs");
 
 //    takes an artist that you searched for shows if they are coming to your town.
 
-var concertThis = function(artist){
-var region = ""
-var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-//console.log (queryUrl);
-console.log(queryUrl);
-axios(queryUrl, function(err,response,body){
-
-    if(!err && response.statusCode === 200){
-    var concertInfo = JSON.parse(body);
-
-
-    outputData(artist + " Concert Information:")
-
-    for (i=0; i< concertInfo.length; i++ ){
+var concertThis = function (artist) {
+  //default is metallica just for fun
+  if (!artist) {
+    artist = "Metallica";
+  }
+  axios
+    .get(
+      "https://rest.bandsintown.com/artists/" +
+        artist +
+        "/events?app_id=codingbootcamp"
+    )
+    .then(function (response) {
+      for (var i = 0; i < response.data.length; i++) {
+        outputData("Venue: " + response.data[i].venue.name);
+        outputData("Location: " + response.data[i].venue.city +
+            ", " +
+            response.data[i].venue.country
+        );
+        outputData("Date: " + moment(response.data[i].datetime).format("L"));
         
-        region = concertInfo[i].venue.region
+      }
+    });
+};
 
-        if (region === " ") {
-            region = concertInfo[i].venue.country
-        }
-        outputData("Venue: " + concertInfo[i].venue.name)
-        outputData("Location: " + concertInfo[i].venue.city + " , " + region);
-        outputData("Date: " + dateFormat(concertInfo[i].datetime, "mm/dd/yyyy"))
-         }
-    }    
-  })
-}
+var spotifyThisSong = function (song) {
+  //default is Ace of Spades
+  if (!song) {
+    song = "The sign Ace of Base";
+  }
 
-var outputData = function(data) {
-    console.log(data);
+  var spotify = new Spotify(keys.spotify);
 
-    fs.appendFile("log.txt" , "\r\n" + data, function(err){
-        if(err){ 
-            return console.log(err);
-        }
-    })
-}
-
-
-var runAction = function(func, data) {
-    switch (func){
-        case "concertThis":
-            concertThis(data)
-            break
-        default:
-            outputData("I don't know what that means. Please try again.")    
+  spotify.search({ type: "track", query: song, limit: 3 }, function (
+    err,
+    data
+  ) {
+    if (err) {
+      return console.log(err);
     }
-}
 
-runAction(process.argv[2],process.argv[3],process.argv[4])
+    var songInfo = data.tracks.items[0];
+    outputData(songInfo.artists[0].name);
+    outputData(songInfo.name);
+    outputData(songInfo.album.name);
+    outputData(songInfo.preview_url);
+  });
+};
+
+var movieThis = function(movie){
+
+    if(!movie){
+        movie = "Mr.+Nobody"
+    }
+
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy")
+    .then(function (response){
+        outputData("Release Year: " + response.data.Title)
+        outputData("Release Year: " + response.data.)
+        outputData("Release Year: " + response.data.)
+        outputData("Release Year: " + response.data.)
+        outputData("Release Year: " + response.data.)
+        outputData("Release Year: " + response.data.)
+        outputData("Release Year: " + response.data.)
+        outputData("Release Year: " + response.data.)
+
+    });
+};
+
+var outputData = function (data) {
+  console.log(data);
+
+  fs.appendFile("log.txt", "\r\n" + data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+};
+
+var runAction = function (func, data) {
+  switch (func) {
+    case "concert-this":
+      concertThis(data);
+      break;
+    case "spotify-this-song":
+      spotifyThisSong(data);
+      break;
+    default:
+      outputData("I don't know what that means. Please try again.");
+  }
+};
+
+runAction(process.argv[2], process.argv[3]);
